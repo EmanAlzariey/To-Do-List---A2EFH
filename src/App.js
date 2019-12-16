51a590be82029cb644d16cda23ec8b32b237f970
EmanAlzariey <55436527+EmanAlzariey@users.noreply.github.com>
2019-12-16 15:22:45 +0300

    Revert "Adding the necessary components"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import TodoHeader from "./TodoHeader";
-import TodoList from "./TodoList";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TodoHeader />
-        <TodoList />
-      </header>
+
     </div>
   );
 }
--- src/TodoHeader.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class TodoHeader extends Component {
-  render() {
-    return <div>TO-A2EFH</div>;
-  }
-}
